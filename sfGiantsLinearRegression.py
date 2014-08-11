038c631ff71946d42b0253c509bcaa020f570a1e
CA4GitHub <flawton@fredricsmacbook.att.net>
2014-08-11 11:55:07 -0700

    Added t-statistics

--- sfGiantsLinearRegression.py
+++ sfGiantsLinearRegression.py
@@ -1,9 +1,35 @@
-#scipy can load .mat files
+#scipy.io can load .mat files
 import scipy.io
 #numpy does linear regression
 import numpy
 #matplotlib.pyplot plots
 import matplotlib.pyplot as plt
+
+'''Use 95% confidence region = 1-alpha => alpha = 0.05
+   Use two-sided test => t_alphaOver2 = t_0.025
+   There are 131 data samples. => degrees of freedom = 131 - 2 = 129
+   Use table to look up t_0.025 for 129 degrees of freedom.
+   I used http://easycalculation.com/statistics/critical-t-test.php
+   Note: could use one-sided test for test if slope is > 0 or < 0
+'''
+t_alphaOver2 = 1.9785
+
+
+def SSX(x):
+	#sum of squares of x
+	xDiffSquared = [(x[i]-numpy.average(x))**2 for i in range(len(x))]
+	return sum(xDiffSquared)
+
+def SSE(y, yEst):
+	#sum of squared errors
+	yDiffSquared = [(y[i]-yEst[i])**2 for i in range(len(y))]
+	return sum(yDiffSquared)
+
+#t-statistic for slope of simple linear regression
+def tStatForSlope(slopeEst,s,ssX):
+	#s**2 = sumSquareErrors(y)/( len(x) - 2 )
+	T = slopeEst/( s/(ssX**0.5) )
+	return T
 
 theData = scipy.io.loadmat('/Users/flawton/Documents/USC/TA Machine Learning/SFGiants_Yr_WL_R_RA.mat')
 theWL = theData['WL']
@@ -31,6 +57,8 @@ A1 = numpy.array( [xdata1, numpy.ones(numSeasons) ] )
 w1 = numpy.linalg.lstsq(A1.T,ydata)[0]
 print(w1)
 yRegEst1 = [w1[0]*xdata1[i] + w1[1] for i in range(numSeasons)]
+tStat1 = tStatForSlope(w1[0],(SSE(ydata,yRegEst1)/(numSeasons-2))**0.5,SSX(xdata1))
+print(tStat1)
 
 plt.plot(xdata1,ydata,'ro',xdata1,yRegEst1,'b-')
 plt.title('SF Win Percentage vs. Runs Scored By SF')
@@ -43,6 +71,8 @@ A2 = numpy.array( [xdata2, numpy.ones(numSeasons) ] )
 w2 = numpy.linalg.lstsq(A2.T,ydata)[0]
 print(w2)
 yRegEst2 = [w2[0]*xdata2[i] + w2[1] for i in range(numSeasons)]
+tStat2 = tStatForSlope(w2[0],(SSE(ydata,yRegEst2)/(numSeasons-2))**0.5,SSX(xdata2))
+print(tStat2)
 
 plt.plot(xdata2,ydata,'ro',xdata2,yRegEst2,'b-')
 plt.title('SF Win Percentage vs. Runs Scored Against SF')
@@ -55,6 +85,8 @@ A3 = numpy.array( [xdata3, numpy.ones(numSeasons) ] )
 w3 = numpy.linalg.lstsq(A3.T,ydata)[0]
 print(w3)
 yRegEst3 = [w3[0]*xdata3[i] + w3[1] for i in range(numSeasons)]
+tStat3 = tStatForSlope(w3[0],(SSE(ydata,yRegEst3)/(numSeasons-2))**0.5,SSX(xdata3))
+print(tStat3)
 
 plt.plot(xdata3,ydata,'ro',xdata3,yRegEst3,'b-')
 plt.title('SF Win Percentage vs. SF Run Differential')
